8d76a0fb118ef256ddc3b7d52e2cc5d27e118a33
Zach McMullen <87542111+ZachMcM@users.noreply.github.com>
2022-12-13 18:11:30 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 const pm2 = require('pm2');
 
-// Start the app
-pm2.connect(function (err) {
-  if (err) {
-    console.error(err);
-    process.exit(2);
-  }
-
-  pm2.start({
-    script: './index.js', // The path to your app's main file
-    name: 'my-app', // The name to use for the app
-    exec_mode: 'fork', // Run the app in a child process
-  }, function (err, apps) {
-    pm2.disconnect(); // Disconnect from pm2
-    if (err) throw err;
-  });
-});
-
 const accountSid = 'account_sid'; // Your Account SID from www.twilio.com/console
 const authToken = 'auth_token'; // Your Auth Token from www.twilio.com/console
 
